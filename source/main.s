7857b6a541a869c7f21ef4e41b30a68280c4a30b
Hilmi A <2ndgenairborne@gmail.com>
2016-05-22 12:58:13 -0600

    Properly works

--- source/main.s
+++ source/main.s
@@ -1,3 +1,9 @@
+//Assignment 1 - CPSC 359 Spring 2016
+//Assignment completed by Hilmi Abou-Saleh and Riaz Ali
+//Program works as expected.
+//Only issue that exists is program will always print 32 bit results
+
+
 .section    .init
 .globl     _start
 
@@ -7,84 +13,194 @@ _start:
 .section .text
 
 
-//r3 is always going to contain the address of string
+//readStringLength - Looks at length of string
+//r3 - contains address of string
+//r0 - returns address of string
+//r1 - returns length of string
 readStringLength:
-		mov r5, #0
-	loop_start:
+		mov r5, #0 //Counter
+	readStringLength_loop:
 		ldrb r4, [r3, r5]
 		add r5, #1
+    //If null then stop loop, otherwise keep going
 		teq r4, #0
-		bne loop_start
-	loop_end:
+		bne readStringLength_loop
+    //loop end
 		mov r0, r3
 		mov r1, r5
-mov pc, lr
+    mov pc, lr
 
-//helper subroutines
 
+//checkArgument - checks if input is AND or OR
+//does printing within function
+//Subroutine isn't general.
+//r7 - returns AND (1), OR (0), or if invalid (-1).
+checkArgument:
+  push {lr}
+  CALoop:
+  //Prints prompt for command
+  ldr r3, =commandprompt
+  bl	readStringLength
+  bl	WriteStringUART
+
+  ldr r0, =commandpromptbuffer
+  mov r1, #256
+  bl ReadLineUART
+  mov r1, r0 //Move length to r1, to match program
+  ldr r0, =commandpromptbuffer
+
+  //If length is greater than 3, then it is not a valid argument
+
+  cmp r1, #3
+  bgt checkNaA
+
+  checkAND:
+  //Check if char 0 = 'A' if not, check OR
+  ldrb r1, [r0]
+  cmp r1, #65
+  bne checkOR
+
+  //Check if char 1 = 'B' if not, check OR
+  ldrb r1, [r0, #1]
+  cmp r1, #78
+  bne checkOR
+
+  //Check if char 2 = 'C' if not, not a command
+  ldrb r1, [r0, #2]
+  cmp r1, #68
+  bne checkNaA
+
+  //If all three match, then check if char 3 is null
+
+  ldrb r1, [r0, #3]
+  cmp r1, #0
+  //If so mov value 0 to r7 and finish subroutine
+  moveq r7, #1
+  beq done
+  //On not equal, it means AND+{char} which is not a valid command
+  bne checkNaA
+
+  checkOR:
+  //Check if char 0 = 'O' if not, not a command
+  ldrb r1, [r0]
+  cmp r1, #79
+  bne checkNaA
+
+  //Check if char 1 = 'R' if not, not a command
+  ldrb r1, [r0, #1]
+  cmp r1, #82
+  bne checkNaA
+  //Check if char 2 = '0' if not, it is not just OR
+  ldrb r1, [r0, #2]
+  cmp r1, #0
+
+  moveq r7, #2
+  beq done
+
+  checkNaA:
+  //Get ready to print invalid command
+  ldr r3, =wrongcommand
+  mov r0, r3
+  bl readStringLength
+  bl	WriteStringUART
+  //Loop until valid command found
+  b CALoop
+
+  done:
+  pop {lr}
+  mov pc, lr
 //r3 - address of ascii characters
 //r2 - return value. binary value
-//r5 - counter
+//r5 - counter starting at the length of the string
+
+
+//convertASCIItoBinary - converts an ASCII string to a binary value
+//r0 - length of ASCII string
+//r2 - contains binary value (-1 if Wrong Number Format [wnf])
 convertASCIItoBinary:
-	mov r5, #0
-  mov r2, #0b0
-	aloop:
-		ldrb r1, [r3, r5]
+  push {lr}
+  //r8 - incremental counter
+  //r5 - decremental counter
+  mov r8, #0
+  mov r2, #0
 
-		teq  r1, #49
-    mov   r6, #1
-		lsleq r6, r5
-		orreq r2, r6
-		addeq r5, #1
-		beq aloop
+  ASCIILoop:
+    //Checks r8 (incremental counter) with length of string
+    cmp     r8, r0
+    beq     endloop
+    //Load starting value (starts at LSB)
+    ldrb  r1, [r3, r5]
+    //Checks if bit + inc counter = 1 (49) or 0 (48)
+    cmp     r1, #49
+    mov     r7, #1
+    //If bit = 1 then this is what happens
+    //ex. r7 = 1 we LSL by r8 (the length) to Get:
+    //    r7 = 1000 (assuming r8 = 3)
+    //    r2 contains the value as of last loop
+    //    Assume r2 = 101
+    //    OR r2 and r7 =  1101
+    lsleq   r7, r8
+    orreq   r2, r7
+    subeq   r5, #1
+    addeq   r8, #1
+    beq     ASCIILoop
 
-		teq r1, #48
-		addeq r5, #1
-		beq aloop
-    bne wnferror
+    //If bit = 0 nothing happens, only counters are updated
+  checkZero:
+    cmp     r1, #48
+    subeq   r5, #1
+    addeq   r8, #1
+    beq     ASCIILoop
 
-		cmp r1, #0
-		beq endloop
-	wnferror:
-		mov r3, r5
-		ldr r3, =wrongnumberformat
-		mov r0, r3
-		b 	readStringLength
-		b	WriteStringUART
 
-		mov r3, r5
-		mov r5, #0
-		b	aloop
+    //Wrong Number Format Error
+    //There is a number that is not a zero or one
+    //Prints error message and r2 returns -1
+  wnferror:
+		ldr r0, =wrongnumberformat
+		bl 	readStringLength
+		bl	WriteStringUART
+    mov r2, #-1
+
 	endloop:
-	mov pc, lr
+    pop {lr}
+    mov pc, lr
 
-
-//r2 - address of binary value
-//r3 - temporary value
-//r4 - return value of buffer
-//r5 - address of ASCII characters
+//convertBinarytoASCII - Takes a binary number and prints the ascii reprensentation
+//r5  - incremental counter
+//r10 - length of binary value
 convertBinarytoASCII:
-	mov r5, #0	//bit counter
-	mov r6, #0
-	bloop:
-		ldrb r4, [r3, r5]
-		cmp  r4, #0 //check for null ter
-		beq bendloop //end of binary value
+  push {r1, r4, r5, r6, lr}
+  mov r5, #0 //Counter
+  //Loads the resulting binary value.
+  ldr r2, =result_storage
+  ldrb r4, [r2]
 
-		sub r4, r4, #48 //convert to ascii value
-		lsl r6, r6, #1
-		add r6, r6, r4
+binary_ASCII_loop:
+  //This rotate prints things in the correct order
+  //Commenting it out will display the correct value reversed
+  ror  r4, #30
 
-		add r5, r5, #1
+  //Checks if r4, being a value is a zero or one
+  tst r4, #1
+  ldrne r0, =printone
+  ldreq r0, =printzero
+  //Printing the zero or one, (one in length always)
+  mov r1, #1
+  bl WriteStringUART
+  //Right shift logical, to get to the next bit and store value
+  lsr r4, r4, #1
+  ldr r2, =result_storage
+  strb r4, [r2]
 
-		b bloop
+  add r5, #1
+  //Branches back until full 32 bit value has been printed
+  cmp r5, #31
+  blt binary_ASCII_loop
 
-	bendloop:
-		mov pc, lr
-
-//r0 - location of buffer
-//r1 - length of string
-
+binary_ASCII_loop_end:
+  pop {r1, r4, r5, r6, lr}
+	mov pc, lr
 
 
 main:
@@ -92,41 +208,107 @@ main:
 	bl		EnableJTAG // Enable JTAG
 	bl		InitUART    // Initialize the UART
 
-  // You can use WriteStringUART and ReadLineUART functions here after the UART initializtion.
+  //Content Begins
 
   //Print Creators of Program
 	ldr r3, =creators
 	bl	readStringLength
 	bl	WriteStringUART
-  //Get argument (AND or OR) and store in r7
-  //bl checkArgument
-  mov r7, #2
 
-  ldr r3, =firstbinary
-  bl readStringLength
-  bl WriteStringUART
+  start_loop:
+  //Get argument (AND or OR) and store in buffer
+  bl checkArgument
+  ldr r8, =logicstorage
+  strb r7, [r8]
 
-  ldr r0, =firstbinarybuffer
-  mov r1, #256
-  bl ReadLineUART
-  //r2 - length of buffer
-  //r3 - address of ascii characters
-  //r4 - return value. binary value
-  mov r2, #256
-  ldr r3, =firstbinarybuffer
-  bl convertASCIItoBinary
+  //Print the first binary value
+  firstbinaryprint:
+    //Prints prompt
+    ldr r3, =firstbinary
+    bl readStringLength
+    bl WriteStringUART
+    //Reads input
+    ldr r0, =firstbinarybuffer
+    mov r1, #256
+    bl ReadLineUART
+    //Subtracts one from length of string
+    mov r5, r0
+    sub r5, #1
 
+    mov r2, #256
+    ldr r3, =firstbinarybuffer
+    //Length of string
+    mov r10, r0
 
+    //Checks if ASCIItoBinary finished successfully
+    bl convertASCIItoBinary
+    cmp r2, #-1
+    beq firstbinaryprint
+    //Store value if success
+    ldr r1, =binarystorage
+    strb r2, [r1]
 
+  //Print the second binary value
+  secondbinaryprint:
+    //Prints prompt
+    ldr r3, =secondbinary
+    bl readStringLength
+    bl WriteStringUART
+    //Read line
+    ldr r0, =secondbinarybuffer
+    mov r1, #256
+    bl ReadLineUART
+
+    mov r5, r0
+    sub r5, #1
+    mov r2, #256
+    ldr r3, =secondbinarybuffer
+    //r10 = first binary value length
+    //Store the larger one's length in r10
+    cmp r0, r10
+    movgt r10, r0
+    bl convertASCIItoBinary
+    cmp r2, #-1
+    beq secondbinaryprint
+
+  command_prep:
+    //Load first binary value in r1
+    ldr r8, =binarystorage
+    ldrb r1, [r8]
+    //Load and/or
+    ldr r8, =logicstorage
+    ldrb r7, [r8]
+    cmp r7, #1
+    bne doOR
+  doAND:
+    and r0,r1 ,r2
+    b end
+  doOR:
+    orr r0, r1,r2
+  end:
+    ldr r2, =result_storage
+    strb r0, [r2]
+
+  check:
+    //Prints value
+    ldr r3, =printvalue
+    bl readStringLength
+    bl WriteStringUART
+    //Convert binary to ascii with r2 being address
+    ldr r2, =result_storage
+    bl convertBinarytoASCII
+    //Restart until program quits.
+    b start_loop
 
 haltLoop$:
 	b	haltLoop$
+
+
 
 .section .data
 .align 4
 
 //Buffers
-
 firstbinarybuffer:
   .rept 256
   .byte 0
@@ -142,6 +324,22 @@ commandpromptbuffer:
   .byte 0
   .endr
 
+binarystorage:
+  .rept 32
+  .byte 0
+  .endr
+
+result_storage:
+  .rept 32
+  .byte 0
+  .endr
+
+logicstorage:
+  .rept 32
+  .byte 0
+  .endr
+
+//Strings
 creators:
 	.asciz "\r\nCreator Names: Riaz Ali and Hilmi Abou-Saleh"
 
@@ -156,8 +354,15 @@ secondbinary:
 
 
 wrongnumberformat:
-	.asciz "\r\nWrong number format!"
+	.asciz "Wrong number format!"
 
+printvalue:
+	.asciz "\r\nThe result is: "
 
 wrongcommand:
-	.asciz "\r\nPlease enter a valid command!"
+	.asciz "Please enter a valid command!"
+
+printone:
+  .asciz "1"
+printzero:
+  .asciz "0"
